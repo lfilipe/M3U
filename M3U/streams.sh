8d11ffd897bf2aac56a59e1ee1d92711c3adca8c
lfilipe <lfilipe@users.noreply.github.com>
2024-12-17 10:53:36 +0000

    Update streams.sh

--- M3U/streams.sh
+++ M3U/streams.sh
@@ -2,6 +2,9 @@
 
 cd M3U
 
+# Copiar o conteúdo de 'my-list.channels' para 'M3UPT.m3u'
+cp my-list.channels M3UPT.m3u
+
 # TVI - update the stream URL of TVI
 
 sed -i "/live_tvi\/live_tvi/ c https://video-auth6.iol.pt/live_tvi/live_tvi/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" M3UPT.m3u
